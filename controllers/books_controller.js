2b1af10ca8c74d845665cd8dbcf76032b5583975
Andrew Olivares <Wrestler533438@gmail.com>
2023-03-02 17:37:14 -0800

    added seed data

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -2,8 +2,62 @@ const express = require('express')
 const Book = require('../models/book.js')
 const book_router = express.Router()
 
+book_router.get('/seed', (req, res) => {
+    Book.insertMany([{
+        "title": "The Shinobi Initiative",
+        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+        "year": 2014,
+        "quantity": 10,
+        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+      },
+      {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": 3,
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+      },
+      {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": 8,
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+      },
+      {
+        "title": "Wâ€RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": 4,
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+      }])
+        .then(res.status(200).json({
+            message: 'Seed successful'
+        }))
+        .catch(res.status(400).json({
+            message: 'Seed unsuccessful'
+        }))
+})
+
+
 book_router.get('/', (req, res) => {
-    res.send('books')
+    Book.find()
+    .then(foundBooks => {
+        res.json(foundBooks)
+    })
+    .catch(err => {
+        res.status(404).send('404', err)
+    })
+})
+
+book_router.get('/:id', (req, res) => {
+    Book.findById(req.params.id)
+    .then(foundBook => {
+        res.json(foundBook)
+    })
+    .catch(err => {
+        res.status(404).send('404', err)
+    })
 })
 
 module.exports = book_router
--- index.js
+++ index.js
@@ -7,23 +7,24 @@ require('dotenv').config()
 const app = express()
 const MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.PORT
+const bookController = require('./controllers/books_controller.js')
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 mongoose.set('strictQuery', true)
 mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> console.log('Connected!'))
 
 // Routes and contoller
 app.get('/', (req, res)=>{
     res.send('Home page')
 })
 
-const bookController = require('./controllers/books_controller.js')
 app.use('/books', bookController)
 
 app.get('*', (req, res) =>{
-    res.status(404).send('Get error/stub')
+    res.status(404).send('404')
 })
 
 app.listen(PORT)
